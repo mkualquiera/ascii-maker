876fd16b0fe09341ed96ae9ca2b938f8ab725de7
Juan Pablo Ossa Zapata <ozjuanpa@gmail.com>
2020-06-08 13:22:42 -0500

    Implemented C version

---
+++ c/src/main.c
@@ -0,0 +1,129 @@
+#if INTERFACE
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "stb_image.h"
+#include "stb_image_resize.h"
+#include <argp.h>
+#include "ascii.h"
+#include "util.h"
+#endif 
+
+#include "main.h"
+
+const char *argp_program_version = "asciimaker C";
+const char *argp_program_bug_address = "ozjuanpa@gmail.com";
+
+static char doc[] = "Ascii maker -- A tool that converts image to ascii art";
+
+static struct argp_option options[] = {
+  {"output", 'o', "FILE", OPTION_ARG_OPTIONAL, "Output to FILE instead of standard output"},
+  {"input", 'i', "FILE", 0, "Input image FILE"},
+  {"color", 'c', 0, OPTION_ARG_OPTIONAL, "Use colors"},
+  {"background", 'b', 0, OPTION_ARG_OPTIONAL, "Also change background of the text"},
+  {"width", 'w', "WIDTH", 0, "Change the width in characters of the final image"},
+  {"explicit", 'e', 0, OPTION_ARG_OPTIONAL, "Don't treat black and white as the default background and foreground colors"},
+  { 0 }
+};
+
+/* Used by main to communicate with parse_opt. */
+struct arguments
+{
+  char *args[2];                /* arg1 & arg2 */
+  char verbose;
+  char set_output;
+  char set_input;
+  char use_color;
+  char use_background; 
+  int width;
+  char set_width;
+  char explicit;
+  char *output_file;
+  char *input_file;
+};
+
+static error_t parse_opt (int key, char *arg, struct argp_state *state)
+{
+  /* Get the input argument from argp_parse, which we
+     know is a pointer to our arguments structure. */
+  struct arguments *arguments = state->input;
+
+  switch (key)
+    {
+    case 'o':
+      arguments->set_input = 1;
+      arguments->output_file = arg;
+      break;
+    case 'i':
+      if( access( arg, R_OK ) != -1 ) {
+        arguments->set_input =1;
+        arguments->input_file = arg;
+      } else {
+        argp_usage(state);
+      }
+      break;
+    case 'w':
+      arguments->set_width = 1;
+      str2int_errno err = str2int(&arguments->width,arg,10);
+      if (err != STR2INT_SUCCESS)
+        argp_usage(state);
+      break;
+    case 'b':
+      arguments->use_background = 1;
+      break;
+    case 'c':
+      arguments->use_color = 1;
+      break;
+    case 'e':
+      arguments->explicit = 1;
+      break;
+
+    case ARGP_KEY_ARG:
+      //if (state->arg_num >= 2)
+        /* Too many arguments. */
+        //argp_usage (state);
+
+      arguments->args[state->arg_num] = arg;
+
+      break;
+
+    case ARGP_KEY_END:
+      if (arguments->set_input == 0)
+        /* Not enough arguments. */
+        argp_usage (state);
+      break;
+
+    default:
+      return ARGP_ERR_UNKNOWN;
+    }
+  return 0;
+}
+
+static struct argp argp = { options, parse_opt, 0, doc };
+
+int main(int argc, char *argv[]) {
+
+  struct arguments arguments;
+
+  /* Default values. */
+  arguments.output_file = "-";
+  arguments.input_file = "-";
+  arguments.set_input = 0;
+  arguments.set_output = 0;
+  arguments.use_color = 0;
+  arguments.use_background = 0;
+  arguments.width = 0;
+  arguments.set_width = 0;
+  arguments.explicit = 0;
+
+  argp_parse (&argp, argc, argv, 0, 0, &arguments);
+
+  int fd = arguments.set_output ? open(arguments.output_file, O_RDONLY) : STDOUT_FILENO;
+
+  ascii_convert(fd, arguments.use_color, arguments.use_background,
+    arguments.set_width, arguments.width, arguments.explicit,
+    arguments.input_file);
+
+  exit(0);
+}
--- main.py
+++ main.py
@@ -3,21 +3,32 @@ import sys
 import math
 import os
 
-from PIL import Image, ImageDraw, ImageFont, ImageChops, ImageStat, ImageOps
+import argparse
 
-import log
+from PIL import Image, ImageDraw, ImageFont, ImageChops, ImageStat, ImageOps, ImageColor
 
-args = sys.argv[1:]
+parser = argparse.ArgumentParser(description="Convert images to ascii art.")
+parser.add_argument("filename",action='store', help="The filename of the image to be converted")
+parser.add_argument("--width", "-w", action='store', type=int, help="The final width in characters")
+parser.add_argument("--invert", "-i", action='store_true', help="Invert image brightness")
+parser.add_argument("--colors", "-c", action='store_true', help="Use colors in the converted image")
+parser.add_argument("--background", "-b", action='store_true', help="Also change background")
+
+args = parser.parse_args()
+
 # get the image
-if len(args) >= 1:
-	base = Image.open(args[-1]).convert('L')
-else:
-	sys.exit()
+try:
+	if args.colors:
+		base = Image.open(args.filename).convert('RGB')
+	else:
+		base = Image.open(args.filename).convert('L')
+except:
+	print("Unable to read image file.")
 
 base_w, base_h = base.size
 font_size = 16
-cell_width = 9.1
-cell_height = 19
+cell_width = 10
+cell_height = 20
 
 # base dimensions in pixels
 base_w, base_h = base.size
@@ -32,16 +43,10 @@ target_cw = width
 # invert image ?
 inverted = False
 
-for a in args[:-1]:
-	# print(a)
-	if a == "--invert":
-		inverted = True
-	elif a.startswith("--width="):
-		segs = a.split("=", 1)
-		target_cw = int(segs[1])
-	# elif a.startswith("--height="):
-	# 	segs = a.split("=", 1)
-	# 	target_ch = int(segs[1])
+inverted = args.invert
+
+if args.width:
+	target_cw = args.width
 
 if inverted:
 	base = ImageOps.invert(base)
@@ -52,6 +57,7 @@ wpercent = (target_pw/float(base.size[0]))
 target_ph = int((float(base.size[1])*float(wpercent)))
 base = base.resize((int(target_pw), int(target_ph)), Image.ANTIALIAS)
 
+
 # base dimensions in cells
 width = math.floor(target_pw / cell_width)
 height = math.floor(target_ph / cell_height)
@@ -59,43 +65,122 @@ height = math.floor(target_ph / cell_height)
 # get a font
 fnt = ImageFont.truetype('fira_code.ttf', font_size)
 
-log.pushOrigin("Ascii Maker")
+if args.colors:
+	color_codes = [
+		"#181818",
+		"#b13c3d",
+		"#78b13c",
+		"#b1943c",
+		"#3c48b1",
+		"#963cb1",
+		"#42a89c",
+		"#cfcfcf",
+		"#4f4f4f",
+		"#ff5658",
+		"#adff56",
+		"#ffd556",
+		"#5668ff",
+		"#d856ff",
+		"#64ffef",
+		"#ffffff"
+	]
+
+	colors = []
+
+	for color_code in color_codes:
+		colors.append(ImageColor.getrgb(color_code))
 
 dictionary = " 0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&\'()*+,-./:;?@[\\]^_`{|}~<=>"
 
-images = {}
-log.printLogNormal("Rendering stamps")
+print(len(dictionary))
+
+exit()
+
+images = []
 for character in dictionary:
-	new_image = Image.new('L', (int(cell_width), int(cell_height)), (255))
-	new_draw = ImageDraw.Draw(new_image)
-	new_draw.text((0,0), character, font=fnt, fill=(0))
-	images[character] = new_image
+	if args.colors:
+		for foreground_color_id in range(len(colors)):
+			skip=False
+			for background_color_id in range(len(colors)):
+				if skip:
+					continue
+				if not args.background:
+					background_color_id = 0
+					skip=True
+				new_image = Image.new('RGB', (int(cell_width), int(cell_height)), color=colors[background_color_id])
+				new_draw = ImageDraw.Draw(new_image)
+				new_draw.text((0,0), character, font=fnt, fill=colors[foreground_color_id])
+				images.append((new_image,character,background_color_id,foreground_color_id))
+				new_image.save("stamps/" + str(ord(character)).zfill(3) + "_" + str(background_color_id).zfill(2) + "_" + str(foreground_color_id).zfill(2) + ".png")
+	else:
+		new_image = Image.new('L', (int(cell_width), int(cell_height)), (255))
+		new_draw = ImageDraw.Draw(new_image)
+		new_draw.text((0,0), character, font=fnt, fill=(0))
+		images.append((new_image,character))
+
+exit()
+
+
+def parse_setup(setup):
+	result = ""
+	if not setup[1] == 7:
+		if setup[1] >= 0 and setup[1] <= 7:
+			result += "\033[" + str(30 + setup[1]) + "m"
+		else:
+			result += "\033[" + str(82 + setup[1]) + "m"
+	if not setup[2] == 0:
+		if setup[2] >= 0 and setup[2] <= 7:
+			result += "\033[" + str(40 + setup[2]) + "m"	
+		else:
+			result += "\033[" + str(92 + setup[2]) + "m"
+	result += setup[0]
+	result += "\033[0m"
+	return result
 
 
 def best_character_at(x, y):
-	best_score = sys.maxsize
-	best_char = " "
 	xx = x * cell_width
 	yy = y * cell_height
 	reference = base.crop((xx, yy, xx+cell_width, yy+cell_height))
 
-	for c in dictionary:
-		difference = ImageChops.difference(reference, images[c])
-		stat = ImageStat.Stat(difference)
-		score = stat.sum[0]
-		if score < best_score:
-			best_score = score
-			best_char = c
-			if score == 0:
-				return best_char
+	if not args.colors:
+		best_score = sys.maxsize
+		best_char = " "
+		for i in images:
+			c = i[1]
+			difference = ImageChops.difference(reference, i[0])
+			stat = ImageStat.Stat(difference)
+			score = stat.sum[0]
+			if score < best_score:
+				best_score = score
+				best_char = c
+				if score == 0:
+					return best_char
+		return best_char
 
-	return best_char
+	else:
+		best_score = sys.maxsize
+		best_setup = (" ", 0, 0)
+		for i in images:
+			if i[3] == i[2]:
+				if i[1] != " ":
+					continue
+			setup = (i[1], i[3], i[2])
+			difference = ImageChops.difference(reference, i[0])
+			stat = ImageStat.Stat(difference)
+			score = stat.sum[0]*stat.sum[0] + stat.sum[1]*stat.sum[1] + stat.sum[2]*stat.sum[2]
+			if score < best_score:
+				best_score = score
+				best_setup = setup
+				if score == 0:
+					return parse_setup(best_setup)
+		return parse_setup(best_setup)
+		
 
 
-log.printLogNormal("Rendering image")
+
 for y in range(height):
 	for x in range(width):
 		sys.stdout.write(best_character_at(x, y))
 	sys.stdout.write("\n")
 
-log.popOrigin()
